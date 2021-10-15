6258a22c46b24be2a4d9df795d6bba9afd9f7957
Arjun Doel <arjundoel@gmail.com>
2021-10-15 22:25:27 +0100

    env var setup

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    OPEN_WEATHER_KEY: process.env.OPEN_WEATHER_KEY
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
+  console.log(process.env.OPEN_WEATHER_KEY)
   return (
     <div>Hello world</div>
   )
