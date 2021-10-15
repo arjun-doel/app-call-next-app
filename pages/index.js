0ae41dea1e3c622d8a898a45af15effa44c7f72a
Arjun Doel <arjundoel@gmail.com>
2021-10-15 22:45:22 +0100

    get request to api success

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import Head from 'next/head'
+import React, { useEffect }  from 'react'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import axios from 'axios'
 
 const Home = () => {
-  console.log(process.env.OPEN_WEATHER_KEY)
+  
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=chicken`)
+      console.log(data)
+      // setFilteredCountries(data)
+    }
+    getData()
+  }, [])
+
   return (
     <div>Hello world</div>
   )
